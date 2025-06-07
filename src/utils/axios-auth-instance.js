84061e2845521050e61b42ffe048b672d01363a8
abdillahsulthan <abdillahsulthan12@gmail.com>
2025-06-07 15:19:02 +0700

    fix: cors

--- src/utils/axios-auth-instance.js
+++ src/utils/axios-auth-instance.js
@@ -1,47 +1,48 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const axiosAuthInstance = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
-});
+  withCredentials: true
+})
 
 axiosAuthInstance.interceptors.request.use(
   (config) => {
-    const token = sessionStorage.getItem('token');
+    const token = sessionStorage.getItem('token')
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`
     }
-    return config;
+    return config
   },
   (error) => Promise.reject(error)
-);
+)
 
 axiosAuthInstance.interceptors.response.use(
   (response) => response,
   async (error) => {
-    const originalRequest = error.config;
+    const originalRequest = error.config
 
     if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
+      originalRequest._retry = true
       try {
-        const tokenData = { old_token: sessionStorage.getItem('token') };
-        const response = await axiosAuthInstance.post('/refresh-token', tokenData);
+        const tokenData = { old_token: sessionStorage.getItem('token') }
+        const response = await axiosAuthInstance.post('/refresh-token', tokenData)
 
         if (response.status === 200) {
-          const newToken = response.data.data.refresh_token;
-          sessionStorage.setItem('token', newToken);
-          axios.defaults.headers.common['Authorization'] = `Bearer ${newToken}`;
-          originalRequest.headers.Authorization = `Bearer ${newToken}`;
+          const newToken = response.data.data.refresh_token
+          sessionStorage.setItem('token', newToken)
+          axios.defaults.headers.common['Authorization'] = `Bearer ${newToken}`
+          originalRequest.headers.Authorization = `Bearer ${newToken}`
         }
 
-        return axios(originalRequest);
+        return axios(originalRequest)
       } catch (refreshError) {
-        console.error('Token refresh error:', refreshError);
-        return Promise.reject(refreshError);
+        console.error('Token refresh error:', refreshError)
+        return Promise.reject(refreshError)
       }
     }
 
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
-export default axiosAuthInstance;
+export default axiosAuthInstance
